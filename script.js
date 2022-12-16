2977357c2e937a944249085d6d8c895ec102a7f5
richardJET <richardtillo@gmail.com>
2022-12-16 15:30:05 -0500

    added alt text to all images

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // When hover remove current selection button style
 // when click change current selection style
-// when click change gallery div that is visible
+
 
 const listItemIndex = li => {
     let i = 0;
@@ -12,9 +12,9 @@ const listItemIndex = li => {
 }
 
 const timelineOl = document.querySelector('.timeline ol');
-console.log(timelineOl);
+
 timelineOl.addEventListener('click', e => {
-    console.log(e.target.parentElement.parentElement);
+    const timelineLi = e.target.parentElement.parentElement;
 
     const galleryDivAll = document.querySelectorAll('.gallery div');
     
@@ -22,6 +22,8 @@ timelineOl.addEventListener('click', e => {
         div.classList.remove('div-select');
     });
     
-    const galleryDivSelect = galleryDivAll[listItemIndex(e.target.parentElement.parentElement)];
+    const galleryDivSelect = galleryDivAll[listItemIndex(timelineLi)];
     galleryDivSelect.classList.add('div-select');
 });
+
+
